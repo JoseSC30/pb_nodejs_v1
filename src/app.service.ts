30b05b47dbabd96c279af1b2655b333138acc90d
jose luis sossa castro <joseluis.universidad2020@gmail.com>
2025-03-09 11:57:27 -0400

    C2 - Cambios en el texto Hello World.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello mundo y otros mundos mas!';
+    return 'Hello mundo y otros mundos mas. Agrego esta line para visualizar los cambios.';
   }
 }
